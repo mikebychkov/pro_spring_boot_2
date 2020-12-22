a7c6d47b73d5cc00dd17f9b51d8cbee7847ac454
mike <mihailbichkov@yandex.ru>
2020-12-22 14:08:23 +0500

    custom login page using mustache

--- todo-simple-security/src/main/java/pro/sb2/todosimplesecurity/config/ToDoSecurityConfig.java
+++ todo-simple-security/src/main/java/pro/sb2/todosimplesecurity/config/ToDoSecurityConfig.java
@@ -1,11 +1,13 @@
 package pro.sb2.todosimplesecurity.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class ToDoSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -28,7 +30,16 @@ public class ToDoSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
+                .requestMatchers(
+                        PathRequest.toStaticResources().atCommonLocations()
+                ).permitAll()
                 .anyRequest().fullyAuthenticated()
+                .and()
+                    .formLogin().loginPage("/login").permitAll()
+                .and()
+                    .logout().logoutRequestMatcher(
+                        new AntPathRequestMatcher("/logout")
+                    ).logoutSuccessUrl("/login")
                 .and()
                 .httpBasic();
     }
